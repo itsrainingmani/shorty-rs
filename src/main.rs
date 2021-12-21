e17e7000ac882822a5af38a7040f4a3e4c11abc7
Manikandan Sundararajan <mani@hey.com>
2021-12-21 03:47:09 -0500

    add tests for valid and invalid requests

--- src/main.rs
+++ src/main.rs
@@ -43,3 +43,50 @@ fn rocket() -> _ {
         .manage(DashMap::<u32, String>::new())
         .mount("/", routes![shorten, redirect])
 }
+
+#[cfg(test)]
+mod tests {
+    use super::rocket;
+    use rocket::{http::Status, local::blocking::Client};
+
+    #[test]
+    fn valid_requests() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client.post("/api/shorten?url=https://duck.com").dispatch();
+
+        assert_eq!(response.status(), Status::Ok);
+
+        let key: u32 = response
+            .into_string()
+            .expect("body")
+            .parse()
+            .expect("valid u32");
+
+        let response = client.get(format!("/{}", key)).dispatch();
+
+        assert_eq!(response.status(), Status::SeeOther);
+
+        let redirect = response
+            .headers()
+            .get_one("Location")
+            .expect("location header");
+
+        assert_eq!(redirect, "https://duck.com")
+    }
+
+    #[test]
+    fn empty_url() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client.post("/api/shorten?url=").dispatch();
+
+        assert_eq!(response.status(), Status::BadRequest);
+    }
+
+    #[test]
+    fn invalid_url() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client.post("/123").dispatch();
+
+        assert_eq!(response.status(), Status::NotFound)
+    }
+}
